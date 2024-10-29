063eeac03ac0b82149b99ea4052b540c037410bd
bxnkz <bankzzx2@gmail.com>
2024-10-29 23:29:46 +0700

    Edit function

--- route/productRoute.js
+++ route/productRoute.js
@@ -1,5 +1,4 @@
 import express from "express";
-import multer from "multer";
 import * as productC from "../controller/productController.js";
 const router = express.Router()
 
@@ -9,4 +8,5 @@ router.get('/products',productC.getAllProduct)
 router.delete('/products/:id',productC.deleteProduct)
 router.put('/products/edit/:id',productC.editProduct)
 router.get('/products/edit/:id',productC.getEditProduct)
+
 export default router
